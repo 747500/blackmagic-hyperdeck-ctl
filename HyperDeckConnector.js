25ad3ac3b5d1e7a6380ee6c3bcedc39e02f2936b
Serge R <747500@gmail.com>
2014-10-08 01:46:00 +0400

    add HyperDeck conversation facility

---
+++ HyperDeckConnector.js
@@ -0,0 +1,93 @@
+
+"use strict";
+
+var util = require('util');
+
+var net = require('net');
+var readline = require('readline');
+var _ = require('underscore');
+
+function HyperDeckConnector(options) {
+
+	var socket = net.connect(options);
+
+	var rd = readline.createInterface({
+		input: socket,
+		terminal: false
+	});
+
+	var cmdQueue = [];
+	var reply = [];
+
+	rd.on('line', function (data) {
+
+		data = data.toString();
+		reply.push(data);
+
+		if (1 === reply.length && data.match(/:$/)) {
+			return;
+		}
+
+		if (1 < reply.length && 0 !== data.length) {
+			return;
+		}
+
+		var text = reply.join('\n');
+		reply = [];
+
+		var parsed = text.match(/^(\d+)\s+([^]*)$/);
+
+		var result = {
+			code: '000',
+			text: ''
+		};
+
+		if (parsed) {
+			result.code = parsed[1];	
+			result.text = parsed[2];
+		}
+		else {
+			result.text = text;
+		}
+
+		if (result.code >= 500) {
+			socket.emit('notify', result);
+			return;
+		}
+
+		var item = cmdQueue.shift();
+
+		result.command = item.command;
+		item.cb(result);
+
+		if (cmdQueue.length) {
+			socket.write(cmdQueue[0].command + '\n');
+		}
+	});
+
+	// interface
+
+	var O = function () {};
+
+	O.prototype = socket;
+
+	O.prototype.command = function (cmd, cb) {
+		var self = this;
+
+		cmdQueue.push({
+			command: cmd,
+			cb: cb
+		});
+
+		if (cmdQueue.length === 1) {
+			self.write(cmdQueue[0].command + '\n');
+		}
+
+	};
+	
+	return new O();
+};
+
+module.exports = HyperDeckConnector;
+
+
---
+++ HyperDeckPool.js
@@ -0,0 +1,176 @@
+
+"use strict";
+
+var util = require('util');
+var events = require('events');
+var _ = require('underscore');
+
+var HyperDeckConnector = require('./HyperDeckConnector.js');
+
+var deckSchema = [
+	'id',
+	'host',
+	'port',
+	'name',
+	'disabled'
+];
+
+function HyperDeckPool() {
+	this.pool = {};
+	this.reconnectDelay = 1000;
+}
+
+HyperDeckPool.prototype = new events.EventEmitter();
+
+HyperDeckPool.prototype.connect = function (params) {
+	var self = this;
+
+	params.socket = new HyperDeckConnector({
+		host: params.host,
+		port: params.port
+	});
+
+	params.socket.once('connect', function () {
+		self.emit('open', _(params).omit('socket'));
+	});
+
+	params.socket.on('error', function (err) {
+		self.emit('notify', {
+			type: 'error',
+			deckId: params.id,
+			message: err
+		});
+	});
+
+	params.socket.on('notify', function (data) {
+		self.emit('notify', {
+			type: 'message',
+			deckId: params.id,
+			message: data
+		});
+	});
+
+	params.socket.once('close', function (had_error) {
+		delete params.socket;
+
+		self.emit('close', params);
+
+		if (params.disabled) {
+			return;
+		}
+
+		if ('object' !== typeof self.pool[params.id]) {
+			self.emit('remove', params);
+			return;
+		}
+
+		setTimeout(function () {
+			self.connect(params);
+		}, self.reconnectDelay);
+	});
+}
+
+HyperDeckPool.prototype.disconnect = function (params) {
+	params.socket.end();
+	params.socket.removeAllListeners();
+	delete params.socket;
+}
+
+HyperDeckPool.prototype.add = function (params) {
+
+	if ('object' === typeof this.pool[params.id]) {
+		console.error('%j exists', params);
+		return;
+	}
+
+	this.pool[params.id] = {
+		id: params.id,
+		name: params.name,
+		host: params.host,
+		port: (params.port || 9993),
+		disabled: (params.disabled || false)
+	};
+
+	this.emit('add', this.pool[params.id]);
+
+	if (this.pool[params.id].disabled) {
+		return;
+	}
+
+	this.connect(this.pool[params.id]);
+}
+
+HyperDeckPool.prototype.remove = function (params) {
+
+	if (!this.pool[params.id]) {
+		console.error('%j not exists', params);
+		return;
+	}
+
+	if (this.pool[params.id].socket) {
+		this.disconnect(this.pool[params.id]);
+	}
+
+	delete this.pool[params.id];
+}
+
+HyperDeckPool.prototype.update = function (params) {
+	var deck = this.pool[params.id];
+	var count = 0;
+
+	if (!deck) {
+		console.error('%j not exists', params);
+		return;
+	}
+
+	deckSchema.forEach(function (k) {
+		if ('undefined' !== typeof params[k]) {
+			deck[k] = params[k];
+			count ++;
+		}
+	});
+
+	if (0 === count ) {
+		return;
+	}
+
+	this.emit('update', _(deck).omit('socket'));
+
+	if (!deck.socket && !deck.disabled) {
+		this.connect(deck);
+		return;
+	}
+
+	if (deck.socket && deck.disabled) {
+		this.disconnect(deck);
+		return;
+	}
+
+	if (deck.socket && deck.host !== deck.socket.remoteAddress) {
+		this.disconnect(deck);
+		return;
+	}
+
+	if (deck.socket && deck.port !== deck.socket.remotePort) {
+		this.disconnect(deck);
+		return;
+	}
+
+}
+
+HyperDeckPool.prototype.command = function (cmd, cb) {
+	_(this.pool).each(function (deck) {
+		if (deck.socket) {
+			deck.socket.command(cmd, function (reply) {
+				cb({
+					type: 'reply',
+					deckId: deck.id,
+					message: reply
+				});
+			});
+		}
+	});
+}
+
+module.exports = HyperDeckPool;
+
